554528f8ed185a07ed6af1541db2147935265eca
der3ki <cristian.moreno.catala@gmail.com>
2021-06-25 15:09:59 +0200

    create component header, and delete all #app styles on others components
    create component header, and delete all #app styles on others components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <Header></Header>
    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import Header from './components/header';
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMobileDetection from 'vue-mobile-detection'
 
 Vue.config.productionTip = false
 
+Vue.use(VueMobileDetection)
 new Vue({
   router,
   store,
