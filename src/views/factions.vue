ddbc60b069f89dd8c1491f8b4df986a633a34d83
Cristian <cristian.moreno.catala@gmail.com>
2021-06-25 14:36:35 +0200

    set routing and create views

---
+++ src/views/factions.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    this is factions
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Factions',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/notFoundComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    not found component
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'notFoundComponent',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
