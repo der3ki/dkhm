98aa269a10ea0efde2f3bfbe21655701956b85a3
der3ki <cristian.moreno.catala@gmail.com>
2021-06-28 23:52:52 +0200

    add banner component and styling header

---
+++ src/components/banner.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="banner">
+        <div>
+            <h1>Bienvenido a DKHM</h1>
+            <button @click="scrollTo()">
+                >
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Banner',
+  data(){
+    return{
+     
+    }
+  },
+  methods:{
+      scrollTo(){
+        window.scroll({top:400,left:0,behavior:'smooth'})
+      }
+  }
+}
+</script>
+
+<style scoped>
+    @font-face {
+    font-family: "SkateBrand";
+    src: local("SkateBrand"),
+    url('../assets/fonts/Skate_Brand.otf') format("truetype");
+    }
+    button{
+        appearance: none;
+        background-color: inherit;
+        width: 67px;
+        height: 67px;
+        border: 1px solid #ffffff;
+        border-radius: 50%;
+        color: #ffffff;
+        transition: all ease .35s;
+        position:absolute;
+        bottom: 50px;
+        left:50%;
+        font: 500 28px/30px "Poppins",sans-serif;
+        -webkit-transform:translateX(-50%) rotate(90deg);
+        -ms-transform:translateX(-50%) rotate(90deg);
+        transform:translateX(-50%) rotate(90deg);
+        cursor:pointer;
+    }
+    .banner{
+        padding:84px 15px 50px;
+        background:url("https://firebasestorage.googleapis.com/v0/b/dkhm-862c9.appspot.com/o/banner_dkhm.jpg?alt=media&token=a848d736-8df9-4a6d-a060-f7e65b436272") center center / cover no-repeat;
+        min-height:367px;
+        position:relative;
+    }
+    .banner > div > h1{
+        font-family: "SkateBrand",sans-serif;
+        font-weight: 800;
+        font-size: 1.8em;
+        line-height: 1.7em;
+        letter-spacing: 0.18em;
+        color: #fff;
+        display: block;
+        text-shadow: 0px 0px 10px rgb(0 0 0 / 35%);
+    }
+    .banner > div > button{
+
+    }
+    @media(min-width:601px){
+        .banner{
+            min-height: 397px;
+        }
+    }
+    @media(min-width:992px){
+        .banner{
+            min-height: 587px;
+        }
+    }
+</style>
