25bc40f743879a8a4065f40e9c056fab5549b0da
der3ki <cristian.moreno.catala@gmail.com>
2021-07-16 12:50:10 +0200

    create faction focus, faction components, store....
    create faction focus, faction components, store....

---
+++ src/views/faction.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <Banner :home="false" :title="getFactionByName.name" :image="getFactionByName.img"></Banner>
+    <actualFaction></actualFaction>
+  </section>
+</template>
+
+<script>
+import Banner from '../components/banner.vue';
+import actualFaction from '../components/actualFaction.vue';
+export default {
+  name: 'faction',
+  components: {
+    Banner,
+    actualFaction
+  },
+  props:{
+      name:String
+  },
+  computed:{
+        getFactionByName(){
+            return this.$store.getters.getFactionByName(this.$route.params.name)
+        },
+      
+    },
+}
+</script>
+
+<style>
+
+</style>
