f4f59c082b1446dafd06d7587fd135daee897e68
der3ki <cristian.moreno.catala@gmail.com>
2021-11-29 18:06:32 +0100

    new team view

--- src/views/team.vue
+++ src/views/team.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-    this is Team
-  </div>
+  <section>
+    <Banner :home="false" title="Team " image="https://firebasestorage.googleapis.com/v0/b/dkhm-862c9.appspot.com/o/ageofsigmarmural.png?alt=media&token=fce2ebad-b15d-4aa7-bfe0-07a1f21c6f53"></Banner>
+  </section>
 </template>
 
 <script>
-
+import Banner from '../components/banner.vue';
 export default {
   name: 'Team',
   components: {
+    Banner
   }
 }
 </script>
