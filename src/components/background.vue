9fd2fb1900ce4f3f504c7ee924994b5e70d26364
der3ki <cristian.moreno.catala@gmail.com>
2022-03-01 16:17:57 +0100

    update background lorevideo

--- src/components/background.vue
+++ src/components/background.vue
@@ -14,7 +14,7 @@
             > aquí</a> </b>
         </p>
         <iframe
-          src="https://www.youtube.com/embed/XFGDe7rLctc"
+          src="https://youtu.be/EE_MmxtpI6g"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
